8dc4e59ead61f61ad795da4368c35fa2dec980fa
AliceEngineer <alice_engineer@yeah.net>
2022-11-07 01:33:07 +0800

    converter
    Signed-off-by: AliceEngineer <alice_engineer@yeah.net>

--- flask_part1/README.md
+++ flask_part1/README.md
@@ -4,3 +4,4 @@
 - [获取配置文件的三种方法](./demo2_config.py)
 - [根据不同的配置文件来设置不通的运行环境](./demo3_project.py)
 - [蓝图的使用](./demo4_buleprint.py)
+- [请求信息固定参数转换器](./demo5_converter.py)
---
+++ flask_part1/demo5_converter.py
@@ -0,0 +1,31 @@
+# coding: utf8
+""" 
+@File: demo5_converter.py
+@Author: Alice(From Chengdu.China)
+@HomePage: https://github.com/AliceEngineerPro
+@CreatedTime: 2022/11/7 1:00
+"""
+
+from flask import Flask
+from werkzeug.routing import BaseConverter
+
+app = Flask(__name__)
+
+# 自定义转化器
+class IphoneNumber(BaseConverter):
+    regex = r'^1[3-9]\d{9}'
+    
+app.url_map.converters['iphone_number'] = IphoneNumber
+
+@app.route('/user/<int:user_id>')
+def index(user_id):
+    return user_id
+
+@app.route('/iphone/<iphone_number:i_id>')
+def get_iphone_number(i_id):
+    return i_id
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
