10db0f869cf0d11781626e5c291af6895b7ee9d4
AliceEngineer <alice_engineer@yeah.net>
2022-11-07 18:59:27 +0800

    自定义cookie喝session

---
+++ flask_part1/demo12_cookie_session.py
@@ -0,0 +1,20 @@
+# coding: utf8
+""" 
+@File: demo12_cookie_session.py
+@Author: Alice(From Chengdu.China)
+@HomePage: https://github.com/AliceEngineerPro
+@CreatedTime: 2022/11/7 18:54
+"""
+
+from flask import Flask, make_response, request
+
+app = Flask(import_name=__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
